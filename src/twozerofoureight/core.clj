33422a696497258b2a844e2e89474793a2828855
Chris Freeman <chris.fy@gmail.com>
2014-04-25 16:33:25 +0100

    Cleaned?

--- src/twozerofoureight/core.clj
+++ src/twozerofoureight/core.clj
@@ -1,21 +1,5 @@
 (ns twozerofoureight.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
-
-(defn merge-row [row]
-  (let [p (partition-by identity row)
-        a (map #(if (> (count %) 1)
-                  (map (fn [coll]  (apply + coll)) (partition 2 2 [] %))
-                  %)
-               p)
-        b (mapcat identity a)]
-    b))
-
-;; idea: store as map - strip values, parition by 4, then can do merge in direction
-
 (def grid {:a1 nil :a2 2 :a3 nil :a4 4
            :b1 nil :b2 2 :b3 8 :b4 4
            :c1 2 :c2 8 :c3 4 :c4 nil
@@ -26,36 +10,40 @@
                 :c1 :c2 :c3 :c4
                 :d1 :d2 :d3 :d4])
 
-(->> grid
-     grid->rows
-     (map #(filter (complement nil?) %))
-     (map merge-row))
-
-(let [rows [[1 2 3 4] [5 6 7 8] [9 10 11 12] [13 14 15 16]]]
-	(for [n (range 4)]
-          (map #(nth % n) rows)))
+(defn merge-row [row]
+  (let [p (partition-by identity row)
+        a (map #(if (> (count %) 1)
+                  (map (fn [coll]  (apply + coll)) (partition 2 2 [] %))
+                  %)
+               p)
+        b (mapcat identity a)]
+    b))
 
 (defn rotate-rows [rows]
   (for [n (range 4)]
     (map #(nth % n) rows)))
 
-[[1 2 3 4]
- [2 3 4 5]]
-
 (defn grid->rows [grid]
   (->> grid
        vals
+;       (map #(filter (complement nil?) %))
        (partition 4)))
 
-(def row [1 2 2 3])
 
+(comment
+  (let [rows [[1 2 3 4] [5 6 7 8] [9 10 11 12] [13 14 15 16]]]
+	(for [n (range 4)]
+          (map #(nth % n) rows)))
+  
+  (->> grid
+       grid->rows
+       (map merge-row)
+       )
 
-[[1 5]
- [6 9]]
-
-[[1 6]
- [5 9]]
-
+  (->> grid
+       vals
+       (map #(filter (complement nil?) %))
+       (partition 4)))
 
 
 (comment "Two parts:
